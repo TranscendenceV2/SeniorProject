a31db27d0e09d128ccdbe54396d9fca16b120ad4
TranscendenceV2 <n00970992@ospreys.unf.edu>
2016-11-09 20:13:04 -0500

    First Commit

---
+++ FirstIteration/Controllers/ChartController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FirstIteration.Controllers
+{
+    public class ChartController : Controller
+    {
+        // GET: Chart
+        public ActionResult Column()
+        {
+            return View();
+        }
+
+        public ActionResult Dashboard()
+        {
+            return View();
+        }
+
+        public JsonResult GetSalesData()
+        {
+            List<Table> sd = new List<Table>();
+            using (MyDatabaseEntities5 dc = new MyDatabaseEntities5())
+            {
+                sd = dc.Tables.OrderBy(a=>a.Year).ToList();
+            }
+
+            var chartData = new object[sd.Count + 1];
+            chartData[0] = new object[]{
+                "Year",
+                "State",
+                "Medicade",
+                "Commercial"
+            };
+
+            int j = 0;
+            foreach( var i in sd)
+            {
+                j++;
+                chartData[j] = new object[] {i.Year, i.State, i.Medicade, i.Commercial };
+            }
+
+            return new JsonResult {Data = chartData, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+        }
+    }
+}
---
+++ FirstIteration/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FirstIteration.Startup))]
+namespace FirstIteration
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
